54fc28bc799cf952ccddb6da34f81203ea536bfe
Lesnov Petr <lesnov,p@gmail.com>
2021-07-15 22:05:12 +0300

    шестая ветка

--- src/service/auth.js
+++ src/service/auth.js
@@ -2,12 +2,23 @@ const bcrypt = require("bcrypt");
 const jsonwebtoken = require("jsonwebtoken");
 const { NotAuthorizedError } = require("../validation");
 const { User } = require("../db");
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const registration = async (req) => {
   const { email, password } = req.body;
 
   const user = new User({ email, password });
 
+  const msg = {
+    to: email,
+    from: "ezhov.kirill98@gmail.com",
+    subject: "hello",
+    text: "hello world",
+    html: "<h1>Hello world</h1>",
+  };
+
+  await sgMail.send(msg);
   await user.save();
 };
 
